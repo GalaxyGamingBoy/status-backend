750be4a790004956d567d1a868b19d0841d2c94f
Marios Mitsios <xrteach@gmail.com>
2024-10-26 13:40:49 -0400

    Update StatusApplication.java

--- src/main/java/xyz/mariosm/status/StatusApplication.java
+++ src/main/java/xyz/mariosm/status/StatusApplication.java
@@ -4,18 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatusApplication {
-//	public static void main(final String[] args) throws Exception {
-//		String hostUrl = "http://localhost:8086";
-//		char[] authToken = System.getenv("INFLUXDB_TOKEN").toCharArray();
-//
-//		try (InfluxDBClient client = InfluxDBClientFactory.create(hostUrl, authToken)) {
-//
-//		}
-//	}
-
+public class StatusApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(StatusApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(serverTestApplication.class);
 	}
 
 }
