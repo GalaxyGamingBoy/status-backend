d999cdb47daa35a6ee820dc6c93ef15b6fad73bd
Marios <xrteach@gmail.com>
2024-10-26 20:52:10 +0300

    chore: Servlet

--- src/main/java/xyz/mariosm/status/StatusApplication.java
+++ src/main/java/xyz/mariosm/status/StatusApplication.java
@@ -9,10 +9,4 @@ public class StatusApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(StatusApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(serverTestApplication.class);
-	}
-
 }
